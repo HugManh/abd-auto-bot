b82d3546e70e3cf610530ad1c3df7a9866bdf5b1
HugManh <manh2909@gmail.com>
2025-06-09 22:00:47 +0700

    feat: set timezone

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const { quotes } = require('./quotes');
 const token = process.env.SLACK_BOT_TOKEN;
 const channelId = process.env.SLACK_CHANNEL_ID;
 if (!token || !channelId) {
-  console.error("Missing SLACK_BOT_TOKEN or SLACK_CHANNEL_ID in .env");
+    console.error("Missing SLACK_BOT_TOKEN or SLACK_CHANNEL_ID in .env");
 }
 console.log(`Slack Bot: ${channelId}`);
 
 const web = new WebClient(token);
+const timezone = "Asia/Ho_Chi_Minh"
 
 async function sendMessage(message) {
     try {
@@ -31,7 +32,7 @@ async function sendMessage(message) {
 cron.schedule('*/1 * * * *', async () => {
     const auth = await client.auth.test();
     console.log(`[${now}] Slack Bot: ${auth}`);
-})
+}, { scheduled: true, timezone: timezone })
 
 // 08:30 sáng, từ thứ 2 đến thứ 6
 cron.schedule('30 8 * * 1-5', () => {
@@ -39,20 +40,20 @@ cron.schedule('30 8 * * 1-5', () => {
     const quote = quotes[dayOfMonth - 1] || 'Không có con đường nào dẫn đến thành công mà không có nỗ lực và công sức. 🛠️';
     const message = `${quote}\n Chúc mọi người có một ngày làm việc hiệu quả!`;
     sendMessage(message);
-});
+}, { scheduled: true, timezone: timezone });
 
 // 13:30 chiều, từ thứ 2 đến thứ 6
 cron.schedule('30 13 * * 1-5', () => {
     sendMessage("Hiện tại ưu tiên về chất lượng code + task, không yêu cầu về tốc độ phải nhanh. Mọi người nhớ làm việc cẩn thận, tỉ mỉ, test kĩ những task mình làm.  🚀");
-});
+}, { scheduled: true, timezone: timezone });
 
 // 17:20 chiều, từ thứ 2 đến thứ 6
 // cron.schedule('20 17 * * 1-5', () => {
 //     sendMessage("Anh em nhớ báo cáo tiến độ hàng ngày nha 🚀\n📌 Link báo cáo: <https://docs.google.com/spreadsheets/d/1qM2UDT8pRcPFS-reqe4-WU3_sVsRQ-LPKc66huIAKag/edit?gid=0#gid=0>");
-// });
+// }, { scheduled: true, timezone: timezone });
 
 // 15:55 chiều, chỉ thứ 6
 cron.schedule('55 15 * * 5', () => {
     sendMessage("Mọi người chuẩn bị vào họp team lúc 5h nhé");
-});
+}, { scheduled: true, timezone: timezone });
 console.log('Slack bot started, waiting for the next scheduled time...');
